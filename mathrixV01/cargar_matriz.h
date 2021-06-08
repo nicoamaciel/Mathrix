969c88f103857de23df9518bba93ecccad6e0a40
Nicolas Maciel <77804530+nicoamaciel@users.noreply.github.com>
2021-06-08 17:50:08 -0300

    ProjectoVersion1.0 - funciones

---
+++ mathrixV01/cargar_matriz.h
@@ -0,0 +1,8 @@
+#ifndef CARGAR_MATRIZ_H_INCLUDED
+#define CARGAR_MATRIZ_H_INCLUDED
+/**CARGAR TABLERO Y MOSTRARLO CON OPCION 1
+*/
+void cargar_tablero (int[][6], int, int);
+void mostrar_tablero (int[][6], int, int);
+
+#endif // CARGAR_MATRIZ_H_INCLUDED
